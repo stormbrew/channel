f355a3fbdfff9e6247977e675534be7f589e217b
Graham <graham-storm-git@ript.net>
2009-11-05 22:19:52 -0700

    Implemented specs for all the node types.
    Some of them don't work yet.

--- stage1/test/parser_spec.rb
+++ stage1/test/parser_spec.rb
@@ -59,5 +59,72 @@ describe Channel::Parser do
 				StringConstant::parse(%Q{'blorp\\'blah'}).should == StringConstant.new(%{blah\\'blorp}, :simple)
 			end
 		end
+		
+		describe Reference do
+			it "should accept a $ value" do
+				Reference::parse(%Q{$blah}).should == Reference.new(%Q{blah}, '$')
+			end
+			it "should accept an @ value" do
+				Reference::parse(%Q{@blah}).should == Reference.new(%Q{blah}, '@')
+			end
+		end
+		
+		describe Tuple do
+			it "should create an empty tuple from an empty string" do
+				Tuple::parse(%Q{}, :file, "\n", nil).should == Tuple.new([], :file)
+			end
+			it "should create a single value tuple from any of the scalar subtypes" do
+				Tuple::parse(%Q{blah}, :file, "\n", nil).should == Tuple.new([BareWord.new('blah')], :file)
+				Tuple::parse(%Q{"blah"}, :file, "\n", nil).should == Tuple.new([StringConstant.new('blah', :complex)], :file)
+				Tuple::parse(%Q{'blah'}, :file, "\n", nil).should == Tuple.new([StringConstant.new('blah', :simple)], :file)
+				Tuple::parse(%Q{$blah}, :file, "\n", nil).should == Tuple.new([Reference.new('blah', '$')], :file)
+				Tuple::parse(%Q{@blah}, :file, "\n", nil).should == Tuple.new([Reference.new('blah', '@')], :file)
+			end
+			it "should be able to have TupleSets within it" do
+				Tuple::parse(%Q|()|, :file, "\n", nil).should == Tuple.new([TupleSet.new([], :comma)], :file)
+				Tuple::parse(%Q|{}|, :file, "\n", nil).should == Tuple.new([TupleSet.new([], :line)], :file)
+			end
+			it "should create composite tuples from multiple values" do
+				Tuple::parse(%Q{blah "blah" $blorp}, :file, "\n", nil).should == Tuple.new([BareWord.new('blah'), StringConstant.new('blah', :complex), Reference.new('blorp', '$')], :file)
+			end
+		end
+		
+		describe TupleSet do
+			it "should create an empty tupleset from empty braces" do
+				TupleSet::parse(%Q|{}|).should == TupleSet.new([], :line)
+				TupleSet::parse(%Q|()|).should == TupleSet.new([], :comma)
+			end
+			it "should create a tupleset with a single tuple in it" do
+				TupleSet::parse(%Q|{blah}|).should == TupleSet.new([Tuple.new([BareWord.new('blah')], :line)], :line)
+				TupleSet::parse(%Q|(blah)|).should == TupleSet.new([Tuple.new([BareWord.new('blah')], :comma)], :comma)
+			end
+			it "should create a tupleset with multiple tuples in it" do
+				TupleSet::parse(%Q|{blah\nblorp}|).should == TupleSet.new([Tuple.new([BareWord.new('blah')], :line), Tuple.new([BareWord.new('blorp')], :line)], :line)
+				TupleSet::parse(%Q|(blah,blorp)|).should == TupleSet.new([Tuple.new([BareWord.new('blah')], :comma), Tuple.new([BareWord.new('blorp')], :comma)], :comma)
+			end
+		end
+	
+		describe Tree do
+			it "should be able to parse a very simple file" do
+				File.open("test/data/really_simple.ch") {|f|
+					Tree::parse(f).should == TupleSet.new([Tuple.new([BareWord.new('a')], :file)], :file)
+				}
+			end
+			it "should be able to parse the baseline sample file" do
+				File.open("test/data/sample.ch") {|f|
+					Tree::parse(f).should == TupleSet.new([Tuple.new([BareWord.new('a')], :file)], :file) # TODO: generate matching data
+				}
+			end
+			it "should be able to parse the closure sample file" do
+				File.open("test/data/closure_sample.ch") {|f|
+					Tree::parse(f).should == TupleSet.new([Tuple.new([BareWord.new('a')], :file)], :file) # TODO: generate matching data
+				}
+			end
+			it "should be able to parse the object sample file" do
+				File.open("test/data/object_sample.ch") {|f|
+					Tree::parse(f).should == TupleSet.new([Tuple.new([BareWord.new('a')], :file)], :file) # TODO: generate matching data
+				}
+			end
+		end
 	end
 end
